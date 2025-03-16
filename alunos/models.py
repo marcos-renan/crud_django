b08e0e0f94673db07120b85b98558e5c7c4f4210
Marcos <devmarcos7@gmail.com>
2025-03-16 11:01:22 -0300

    salvando dados no banco de dados

--- alunos/models.py
+++ alunos/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#model aluno, cria uma tabela no banco de dados
 class Aluno(models.Model):
+    #valores da tabela
     nome = models.CharField(max_length=255)
     email = models.EmailField()
-    
-def __str__(self):
-    return self.nome
+    #stringifica o nome
+    def __str__(self):
+        return self.nome
--- alunos/views.py
+++ alunos/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Aluno
 
 # Create your views here.
 def criar_aluno(request):
-    return render(request, 'criar_aluno.html')
+    #caso o metodo seja get
+    if request.method == 'GET':
+        #retorne a pagina inicial
+        return render(request, 'criar_aluno.html')
+    #caso seja posr
+    elif request.method =='POST':
+        #capture os dados inseridos nos campos
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        #armazene eles nas variaveis da model
+        aluno = Aluno(
+            nome = nome,
+            email = email,
+        )
+        #salve no banco de dados
+        aluno.save()
+        #redirecione para a pagina inicial
+        return redirect('criar_aluno')
+        
